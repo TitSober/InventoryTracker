af34dd9dfb006e00840151aeaed9b1a8656f31bb
TitSober <45947463+TitSober@users.noreply.github.com>
2021-01-12 08:39:00 +0100

    Add files via upload

--- backend/add-to-db.php
+++ backend/add-to-db.php
@@ -1,21 +1,60 @@
 <?php
 include "db_conn.php";
 session_start();
-if(isset($_SESSION['id']) && $_POST['dropdown'] ){
+if(isset($_SESSION['id']) ){
 $id = $_SESSION['id'];
-$oprema_name = $_POST['dropdown'];// ni id
+/*
 $oprema_sql = "SELECT * from oprema where ime_opreme = '$oprema_name'";
-$oprema_result = mysqli_query($conn, $oprema_sql);
+$oprema_result = mysqli_query($conn, $oprema_sql);*/
 $entry_date = $_POST['edate'];
 $return_date = $_POST['return-date'];
 //naredi da se spremeni taken bool v opremi
+//var_dump($_POST);
 
-while($row = mysqli_fetch_array($oprema_result)){
-    $oprema_id = $row['oprema_id'];
+if($_POST['return-date'] < $_POST['edate'] ) {
+
+    header("Location: ../home.php?error=Vnesi veljaven datum");
+    exit();
+}else if(empty($_POST['edate']) || empty($_POST['return-date'])){
+    header("Location: ../home.php?error= Vnesi vse parametre");
+    exit();
+}else{
+    $count_sql = "SELECT oprema_id from oprema";
+    $count_result = mysqli_query($conn, $count_sql);
+    $count_array = array();
+    while($row = mysqli_fetch_array($count_result)){
+        array_push($count_array,$row['oprema_id']);
+    }
+    
+    
+    for($i = 1; $i< max($count_array)+1; $i++){
+        if(isset($_POST[strval($i)])){
+        
+            $cur_id = $_POST[strval($i)];
+            $sql = "INSERT INTO sposoja (user_id, oprema_id, datum_sposoje,datum_vrnitve) values ('$id','$cur_id','$entry_date','$return_date');";
+            $result = mysqli_query($conn, $sql);
+            $update_oprema = "UPDATE oprema SET is_taken = 1 Where oprema_id = '$cur_id'";
+            $result_update = mysqli_query($conn, $update_oprema);
+            if(!$result && !$result_update){
+                header("Location: ../home.php?err= something has gone bananas");
+                exit();}
+            
+
+        }
+    }
+    
+    header("Location: ../home.php?success=Sposoja je bila uspešna");
+    exit();
+    
 }
 
 
 
+
+
+
+
+/*
 if($_POST['return-date'] < $_POST['edate'] ) {
 
     header("Location: home.php?error=Vnesi veljaven datum");
@@ -36,11 +75,11 @@ if($_POST['return-date'] < $_POST['edate'] ) {
         exit();
         /*echo $update_oprema;
         echo " <br>";
-        echo mysqli_error($conn);*/
+        echo mysqli_error($conn);
         
     }
 }
-
+*/
 
 
 
